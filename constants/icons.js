105a361978e66c95497738e22a85944e5a50a17a
andrii.stetsiuk <zmontee@gmail.com>
2023-10-23 16:04:33 +0300

    setup complete

---
+++ constants/icons.js
@@ -0,0 +1,23 @@
+import heart from "./heart.png";
+import menu from "./menu.png";
+import search from "./search.png";
+import filter from "./filter.png";
+import left from "./left.png";
+import heartOutline from "./heart-ol.png";
+import share from "./share.png";
+import location from "./location.png";
+import chevronLeft from './chevron-left.png'
+import chevronRight from './chevron-right.png'
+
+export default {
+  heart,
+  menu,
+  search,
+  filter,
+  left,
+  heartOutline,
+  share,
+  location,
+  chevronLeft,
+  chevronRight
+};
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "./kemal.jpg";
+
+export default {
+  profile,
+};
