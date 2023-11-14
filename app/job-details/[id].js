d27958b9ca66702423d296d32f9136ebfbe24f99
andrii.stetsiuk <zmontee@gmail.com>
2023-11-14 23:59:54 +0200

    job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -8,8 +8,16 @@ import {
 import { Stack, useRouter, useSearchParams } from 'expo-router';
 import useFetch from '../../hook/useFetch';
 import { COLORS, icons, SIZES } from '../../constants';
-import { Company, JobTabs, ScreenHeaderBtn } from '../../components';
-import { useState } from 'react';
+import {
+  Company,
+  JobAbout,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from '../../components';
+import { useCallback, useState } from 'react';
+
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 const JobDetails = () => {
   const params = useSearchParams();
@@ -20,8 +28,42 @@ const JobDetails = () => {
   });
 
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState('About');
+
+  const changeTab = useCallback((tab) => {
+    setActiveTab(tab);
+  }, [setActiveTab]);
 
   const onRefresh = () => {};
+
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title='Qualifications'
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+
+      case "About":
+        return (
+          <JobAbout info={data[0].job_description ?? "No data provided"} />
+        );
+
+      case "Responsibilities":
+        return (
+          <Specifics
+            title='Responsibilities'
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
+
+      default:
+        return null;
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -48,10 +90,12 @@ const JobDetails = () => {
       <>
         <ScrollView
           showsVerticalScrollIndicator={false}
-          refreshControl={<RefreshControl
-                            refreshing={refreshing}
-                            onRefresh={onRefresh}
-                          />}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+            />
+        }
         >
           {isLoading ? (
             <ActivityIndicator size="large" color={COLORS.primary} />
@@ -68,7 +112,9 @@ const JobDetails = () => {
                   location={data[0].job_country}
                 />
 
-                <JobTabs />
+                <JobTabs tabs={tabs} activeTab={activeTab} changeTab={changeTab} />
+
+                {displayTabContent()}
               </View>
           )
           }
